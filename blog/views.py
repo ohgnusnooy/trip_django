5e5ab375caad56955b914e7518eec220e3fe90bb
ohgnusnooy <ysh01022321@gmail.com>
2023-01-28 10:47:06 +0900

    사이트 뼈대 구축 완료

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+
+from .models import Post
+from django.views.generic import ListView,DetailView
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
