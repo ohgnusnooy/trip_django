a83a4c1be2c1ca9aafcc9287800b9eeb5afc7229
ohgnusnooy <ysh01022321@gmail.com>
2023-01-30 10:51:45 +0900

    카테고리 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
+
+#127.0.0.1/admin 으로 접속하면 Blog에 Posts 관리 가능하게 해주는 코드
 admin.site.register(Post)
-# Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+#127.0.0.1/admin 으로 접속하면 Blog에 Categorys 관리 가능하게 해주는 코드
+admin.site.register(Category, CategoryAdmin)
