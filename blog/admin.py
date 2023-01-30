639ce9627b9aced986cb4f78bf72644368a96142
ohgnusnooy <ysh01022321@gmail.com>
2023-01-30 15:45:57 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from .models import Post, Category,Tag
+from .models import Post, Category, Tag
 
-
-#127.0.0.1/admin 으로 접속하면 Blog에 Posts 관리 가능하게 해주는 코드
+# 127.0.0.1/admin 으로 접속하면 Blog에 Posts 관리 가능하게 해주는 코드
 admin.site.register(Post)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
-#127.0.0.1/admin 으로 접속하면 Blog에 Categorys 관리 가능하게 해주는 코드
-admin.site.register(Category, CategoryAdmin)
 
 class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Tag,TagAdmin)
+
+# 127.0.0.1/admin 으로 접속하면 Blog에 Categorys 관리 가능하게 해주는 코드
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Tag, TagAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from .models import Post, Category
-from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from .models import Post, Category,Tag
+from django.views.generic import ListView, DetailView,CreateView
+from django.shortcuts import render,redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostList(ListView):
     model = Post
@@ -12,6 +13,17 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 
 class PostDetail(DetailView):
@@ -40,5 +52,19 @@ def category_page(request, slug):
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
