9123acdbd83f81b8f1f9f1c0cdcfb60386b2ae17
ohgnusnooy <ysh01022321@gmail.com>
2023-01-31 14:01:15 +0900

    댓글 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag,Comment
 from markdownx.admin import MarkdownxModelAdmin
 
 # 127.0.0.1/admin 으로 접속하면 Blog에 Posts 관리 가능하게 해주는 코드
@@ -16,5 +16,5 @@ class TagAdmin(admin.ModelAdmin):
 
 # 127.0.0.1/admin 으로 접속하면 Blog에 Categorys 관리 가능하게 해주는 코드
 admin.site.register(Category, CategoryAdmin)
-
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
