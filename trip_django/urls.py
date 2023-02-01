4833198476d1f9070fcdfe4c6e49f1d72a3a64ed
ohgnusnooy <ysh01022321@gmail.com>
2023-02-01 17:57:56 +0900

    도커에 gunicorn 적용

--- trip_django/urls.py
+++ trip_django/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include('single_pages.urls')),
     path('accounts/',include('allauth.urls')),
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
